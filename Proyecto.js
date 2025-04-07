5a9809ce970177a6b8db51ee3e541afd7a163e53
Enzo <enzocayeta@gmail.com>
2025-04-07 19:19:06 -0300

    actualizado Proyecto.js

--- Proyecto.js
+++ Proyecto.js
@@ -13,6 +13,12 @@ class Proyecto {
 
   mostrarTareas() {
     this.tareas.forEach((tarea) => tarea.mostrarTarea());
+
+    const costoTotal = this.tareas.reduce(
+      (acum, tarea) => acum + tarea.getCosto(1000),
+      0
+    );
+    console.log(`Costo Total del Proyecto: $${costoTotal}`);
   }
 
   cleanTareas() {
